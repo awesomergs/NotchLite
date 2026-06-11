f8bb9f50a09c0ae0b2af00dbf81ce26d8dd0084a
awesomergs <90956516+awesomergs@users.noreply.github.com>
2026-06-11 23:20:22 +0530

    Initial Commit

---
+++ NotchLite/NotchLiteApp.swift
@@ -0,0 +1,17 @@
+//
+//  NotchLiteApp.swift
+//  NotchLite
+//
+//  Created by Rohan George on 6/11/26.
+//
+
+import SwiftUI
+
+@main
+struct NotchLiteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
